547bd0f336b41fde518e0fcb53f2db814593b2e5
Sahilshowkat786 <sahilshowkatsofi@gmail.com>
2026-09-24 18:11:38 +0530

    unused delete

--- 03oop/06accessModifers.py
+++ 03oop/06accessModifers.py
@@ -29,5 +29,36 @@ class Bank:
 c=Bank("sahil","saving",7765)
 # print(c.pin)
 # print(c._pin)
-print(c._Bank__pin)
+print(c._Bank__pin) #name namgling
 
+# code from chart Gpt to learn 
+
+# class Student:
+
+#     def __init__(self, name, age, marks):
+#         self.name = name          # Public
+#         self._age = age           # Protected
+#         self.__marks = marks      # Private → Name Mangling
+
+#     def show_details(self):
+#         print("Name:", self.name)
+#         print("Age:", self._age)
+#         print("Marks:", self.__marks)
+
+# # Object
+# s1 = Student("Sahil", 20, 90)
+
+# # 1. Public
+# print(s1.name)
+
+# # 2. Protected
+# print(s1._age)
+
+# # 3. Private
+# # print(s1.__marks)       # ❌ AttributeError
+
+# # 4. Name Mangling
+# print(s1._Student__marks) # ✅ Works
+
+# # Method can access private variable
+# s1.show_details()
